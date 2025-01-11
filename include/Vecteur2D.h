9cb8d34a07b0562a64b1cc8382ea554e3c260e93
SIIR3X <125802911+SIIR3X@users.noreply.github.com>
2025-01-11 15:30:22 +0100

    feat: amélioration de la classe Vecteur2D

--- include/Vecteur2D.h
+++ include/Vecteur2D.h
@@ -1,5 +1,7 @@
-#pragma once
+#ifndef VECTEUR2D_H
+#define VECTEUR2D_H
 
+#include <cmath>
 #include <string>
 #include <sstream>
 
@@ -14,45 +16,86 @@ public:
 	explicit Vecteur2D(const double &x = 0, const double &y = 0)
 		: x(x), y(y) {}
 
-	inline double determinant(const Vecteur2D &u) const;
-
-	inline const Vecteur2D operator + (const double &a) const;
-
-	inline const Vecteur2D operator - () const;
-
-	inline const Vecteur2D operator * (const double &a) const;
-
-	inline operator string() const;
-
-	friend ostream& operator << (ostream &os, const Vecteur2D &u)
+	double determinant(const Vecteur2D &u) const
 	{
-		return os << string(u);
+		return x*u.y - y*u.x;
 	}
+
+	double norme() const
+	{
+		return sqrt(normeCarree());
+	}
+
+	double normeCarree() const
+	{
+		return x*x + y*y;
+	}
+
+	const Vecteur2D operator + (const double &a) const;
+
+	const Vecteur2D operator + (const Vecteur2D &u) const;
+
+	const Vecteur2D& operator += (const Vecteur2D &u);
+
+	const Vecteur2D operator - () const;
+
+	const Vecteur2D operator - (const Vecteur2D &u) const;
+
+	const Vecteur2D operator * (const double &a) const;
+
+	const Vecteur2D operator * (const Vecteur2D &u) const;
+
+	operator string() const;
+
 }; // class Vecteur2D
 
-double Vecteur2D::determinant(const Vecteur2D &u) const
-{
-	return x*u.y - y*u.x;
-}
-
-const Vecteur2D Vecteur2D::operator + (const double &a) const
+inline const Vecteur2D Vecteur2D::operator + (const double &a) const
 {
 	return Vecteur2D(x+a, y+a);
 }
 
-const Vecteur2D Vecteur2D::operator - () const
+inline const Vecteur2D Vecteur2D::operator + (const Vecteur2D &u) const
+{
+	return Vecteur2D(x+u.x, y+u.y);
+}
+
+inline const Vecteur2D& Vecteur2D::operator += (const Vecteur2D &u)
+{
+	x += u.x;
+	y += u.y;
+	return *this;
+}
+
+inline const Vecteur2D Vecteur2D::operator - () const
 {
 	return Vecteur2D(-x, -y);
 }
 
-const Vecteur2D Vecteur2D::operator * (const double &a) const
+inline const Vecteur2D Vecteur2D::operator - (const Vecteur2D &u) const
+{
+	return Vecteur2D(x-u.x, y-u.y);
+}
+
+inline const Vecteur2D Vecteur2D::operator * (const double &a) const
 {
 	return Vecteur2D(x*a, y*a);
 }
 
-Vecteur2D::operator string() const
+inline const Vecteur2D Vecteur2D::operator * (const Vecteur2D &u) const
+{
+	return Vecteur2D(x*u.x, y*u.y);
+}
+
+inline Vecteur2D::operator string() const
 {
 	ostringstream oss;
 	oss << "(" << x << ", " << y << ")";
 	return oss.str();
 }
+
+inline ostream& operator << (ostream& os, const Vecteur2D &v)
+{
+	return os << string(v);
+}
+
+#endif // VECTEUR2D_H
