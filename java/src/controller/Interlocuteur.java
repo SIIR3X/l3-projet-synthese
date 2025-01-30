1c7d2fa9ee98f283066c87baa3e6b5c3abc5f646
Lezer Léo <lezerleo7@gmail.com>
2025-01-30 16:05:03 +0100

    feat: classes pour la gestion de la connexion tcp/ip au serveur

---
+++ java/src/controller/Interlocuteur.java
@@ -0,0 +1,39 @@
+package src.controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.io.IOException;
+
+public class Interlocuteur extends Thread {
+	PrintStream fluxSortant;
+	BufferedReader fluxEntrant;
+	int noClient;
+
+	public Interlocuteur(Socket client, int noClient) throws IOException {
+		this.fluxSortant = new PrintStream(client.getOutputStream());
+		this.fluxEntrant = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		this.noClient = noClient;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("interlocuteur pret pour le n"+ this.noClient);
+
+		while(!this.isInterrupted()) {
+			String requete = null;
+			try {
+				requete = this.fluxEntrant.readLine();
+			}
+			catch (IOException e) {
+
+			}
+			System.out.println("le client n°"+ noClient + "a envoye " + requete);
+
+			String input = requete;
+
+			//Gestion de la lecture et du COR
+		}
+	}
+}
---
+++ java/src/controller/serverManager.java
@@ -0,0 +1,25 @@
+package src.controller;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class serverManager {
+	private static final int portServeur = 900;
+	private static int noClient = -1;
+
+	public static void main(String[] args) {
+		try (ServerSocket serveur = new ServerSocket(portServeur)) {
+			while(true) {
+				Socket client = serveur.accept();
+				++noClient;
+				Interlocuteur interlocuteur = new Interlocuteur(client, noClient);
+				interlocuteur.start();
+			}
+		}
+		catch (IOException e) {
+
+		}
+	}
+
+}
