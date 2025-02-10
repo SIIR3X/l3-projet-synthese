7e28d672d503d5514150990f0db969e24256bb4f
SIIR3X <125802911+SIIR3X@users.noreply.github.com>
2025-02-10 13:36:26 +0100

    fix: modification de l'envoie des requêtes dans VisiteurDessinerTCP

--- include/design_patterns/visiteur/VisiteurDessinerTCP.h
+++ include/design_patterns/visiteur/VisiteurDessinerTCP.h
@@ -1,11 +1,9 @@
 #ifndef VISITEUR_DESSINER_TCP_H
 #define VISITEUR_DESSINER_TCP_H
 
+#include "Client.h"
+#include "graphique/Viewport.h"
 #include "design_patterns/visiteur/VisiteurForme.h"
-#include <string>
-#include <sstream>
-
-using namespace std;
 
 /**
  * @brief Classe VisiteurDessinerTCP permettant de dessiner des formes en TCP.
@@ -13,12 +11,39 @@ using namespace std;
 class VisiteurDessinerTCP : public VisiteurForme
 {
 private:
-	stringstream _buffer;
-	bool _dansGroupe = false;
+	Client& _client; /**< Client TCP. */
+	Viewport* _viewport; /**< Viewport de dessin. */
+	bool _dansGroupe = false; /**< Flag indiquant si on est dans un groupe. */
 
-	void envoyerDonnees();
+	/**
+	 * @brief Crée l'entête du paquet à envoyer.
+	 * @param couleur Couleur de la forme.
+	 * @return Entête du paquet.
+	 */
+	const char* creerEntete(Couleur couleur) const;
+
+	/**
+	 * @brief Envoie un paquet.
+	 * @param paquet Paquet à envoyer.
+	 */
+	void envoyerPaquet(const char* paquet) const;
+
+	/**
+	 * @brief Envoie une forme simple.
+	 * @param f Forme à envoyer.
+	 */
+	void envoyerFormeSimple(const Forme* forme) const;
 
 public:
+	/**
+	 * @brief Constructeur de VisiteurDessinerTCP.
+	 * @param v Viewport de dessin.
+	 */
+	VisiteurDessinerTCP(Viewport* v)
+		: _viewport(v), _client(Client::getInstance()) {}
+
+	void setViewport(Viewport* v) { _viewport = v; }
+
 	virtual void visiter(Cercle* c) override;
 	virtual void visiter(Segment* s) override;
 	virtual void visiter(Triangle* t) override;
@@ -26,17 +51,40 @@ public:
 	virtual void visiter(Groupe* g) override;
 }; // class VisiteurDessinerTCP
 
-inline void VisiteurDessinerTCP::envoyerDonnees()
+inline const char* VisiteurDessinerTCP::creerEntete(Couleur couleur) const
 {
-	// Si le buffer n'est pas vide, alors on envoie les données
-	if (!_buffer.str().empty())
-	{
-		// Envoie de la requête TCP
-		// send(_buffer)
+	char entete[100];
 
-		_buffer.str("");
-		_buffer.clear();
+	// On construit l'entête (largeur hauteur couleur)
+	sprintf(entete, "%d %d %d", _viewport->largeurEcran(), _viewport->hauteurEcran(), couleur);
+
+	return entete;
+}
+
+inline void VisiteurDessinerTCP::envoyerPaquet(const char* paquet) const
+{
+	// On envoie le paquet à l'aide du client TCP
+	_client.send_request(paquet);
+}
+
+inline void VisiteurDessinerTCP::envoyerFormeSimple(const Forme* forme) const
+{
+	// Si on n'est pas dans un groupe, on envoie l'entête et la forme
+	if (!_dansGroupe)
+	{
+		// On se connecte au serveur
+		_client.connect_to_server();
+
+		// On envoie l'entête et la forme
+		envoyerPaquet(creerEntete(forme->couleur()));
+		envoyerPaquet(string(*forme).c_str());
+
+		// On se déconnecte du serveur
+		_client.shutdown_connection();
 	}
+	// Sinon, on envoie seulement la forme (visiter groupe se charge déjà d'ouvrir et fermer la connexion et de gérer l'entête)
+	else
+		envoyerPaquet(string(*forme).c_str());
 }
 
 #endif // VISITEUR_DESSINER_TCP_H
